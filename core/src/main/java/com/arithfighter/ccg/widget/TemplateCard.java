e7c60f83539c42b0f7ccab52ec8e832f16fa29f1
catchball <palefever98@gmail.com>
2022-01-19 15:46:05 +0800

    debug update cardY when touching the card

--- core/src/main/java/com/arithfighter/ccg/widget/TemplateCard.java
+++ core/src/main/java/com/arithfighter/ccg/widget/TemplateCard.java
@@ -13,7 +13,9 @@ public class TemplateCard {
     float cardY;
     float cardWidth;
     float cardHeight;
-    enum CardState{ACTIVE, INACTIVE}
+
+    enum CardState {ACTIVE, INACTIVE}
+
     CardState state = CardState.INACTIVE;
     Sprite card;
     Font text;
@@ -23,48 +25,52 @@ public class TemplateCard {
                         Color color, Texture texture, String number) {
         this.initX = initX;
         this.initY = initY;
-        cardWidth = texture.getWidth()*scale;
-        cardHeight = texture.getHeight()*scale;
+        cardWidth = texture.getWidth() * scale;
+        cardHeight = texture.getHeight() * scale;
         cardX = initX;
         cardY = initY;
         this.number = number;
 
         card = new Sprite(texture);
         card.setColor(color);
-        card.setPosition(initX,initY);
-        card.setSize(cardWidth,cardHeight);
+        card.setPosition(initX, initY);
+        card.setSize(cardWidth, cardHeight);
 
         text = new Font(30);
     }
 
-    public float getWidth(){
+    public float getWidth() {
         return cardWidth;
     }
 
     public void draw(SpriteBatch batch, float x, float y) {
-        float numberX = cardX+10;
-        float numberY = cardY+cardHeight;
-
-        checkTouchOn(batch, x, y);
         checkOutOfWindow();
-        text.setColor(Color.YELLOW);
-        text.draw(batch, number, numberX,numberY);
+        if (isOnCard(x, y))
+            updateWhenTouched();
+        else
+            resetCard();
+        card.setPosition(cardX, cardY);
+        card.draw(batch);
+        drawNumber(batch);
     }
 
-    public void dispose(){
+    public void dispose() {
         text.dispose();
     }
 
-    private void checkTouchOn(SpriteBatch batch, float x, float y) {
-        float movingDis = 15;
-        if (isOnCard(x,y))
-            card.setPosition(cardX,cardY+movingDis);
-        else
-            card.setPosition(cardX,cardY);
-        card.draw(batch);
+    private void updateWhenTouched() {
+        if (cardY < initY + 15)
+            cardY++;
     }
 
-    private void checkOutOfWindow(){
+    private void drawNumber(SpriteBatch batch) {
+        float numberX = cardX + 10;
+        float numberY = cardY + cardHeight;
+        text.setColor(Color.YELLOW);
+        text.draw(batch, number, numberX, numberY);
+    }
+
+    private void checkOutOfWindow() {
         float limitX = Gdx.graphics.getWidth() - cardWidth;
         float limitY = Gdx.graphics.getHeight() - cardHeight;
 
@@ -84,30 +90,31 @@ public class TemplateCard {
 
     public void updateWhenDrag(float x, float y) {
         if (isActive())
-            updatePosition(x - cardWidth / 2,y - cardHeight / 2);
+            updatePosition(x - cardWidth / 2, y - cardHeight / 2);
     }
 
-    public void resetCard(){
+    public void resetCard() {
         updatePosition(initX, initY);
         state = CardState.INACTIVE;
     }
 
-    public void checkActive(float mouseX, float mouseY){
+    public void checkActive(float mouseX, float mouseY) {
         if (isOnCard(mouseX, mouseY))
             state = CardState.ACTIVE;
     }
 
-    public boolean isActive(){
+    public boolean isActive() {
         return state == CardState.ACTIVE;
     }
 
-    private void updatePosition(float x, float y){
+    private void updatePosition(float x, float y) {
         cardX = x;
         cardY = y;
     }
 
     private boolean isOnCard(float x, float y) {
-        return x > cardX && x < cardX + cardWidth &&
-                y > cardY && y < cardY + cardHeight;
+        int tolerance = 25;
+        return x > cardX-tolerance && x < cardX + cardWidth+tolerance &&
+                y > cardY-tolerance && y < cardY + cardHeight+tolerance;
     }
 }
