17589dcb33db4eefac5f1b07e9f0797ccdc4f1e1
catchball <palefever98@gmail.com>
2022-01-20 15:01:46 +0800

    add NumberBox and delete SpriteAttribution

---
+++ core/src/main/java/com/arithfighter/ccg/widget/NumberBox.java
@@ -0,0 +1,39 @@
+package com.arithfighter.ccg.widget;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class NumberBox {
+    Font text;
+    Sprite box;
+    float x, y, width, height;
+    int scale = 6;
+    int fontSize = 32;
+
+    public NumberBox(Texture texture, float initX, float initY){
+        text = new Font(fontSize);
+
+        x = initX;
+        y = initY;
+        width = texture.getWidth()*scale;
+        height = texture.getHeight()*scale;
+
+        box = new Sprite(texture);
+        box.setPosition(x,y);
+        box.setSize(width, height);
+    }
+
+    public void draw(int number, SpriteBatch batch){
+        box.setColor(Color.TEAL);
+        box.draw(batch);
+
+        String content = String.valueOf(number);
+        float textX = x+width/2-content.length()*fontSize/2f;
+        float textY = y+(height+fontSize)/2;
+        
+        text.setColor(Color.WHITE);
+        text.draw(batch, content, textX, textY);
+    }
+}
--- core/src/main/java/com/arithfighter/ccg/widget/SumDisplacer.java
+++ core/src/main/java/com/arithfighter/ccg/widget/SumDisplacer.java
@@ -8,29 +8,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class SumDisplacer {
     Font text;
     Sprite sumDisplayBlock;
-    float blockX, blockY, blockWidth, blockHeight;
+    float x, y, width, height;
     int scale = 6;
     int fontSize = 32;
     
     public SumDisplacer(Texture texture, float initX, float initY){
         text = new Font(fontSize);
 
-        blockX = initX;
-        blockY = initY;
-        blockWidth = texture.getWidth()*scale;
-        blockHeight = texture.getHeight()*scale;
+        x = initX;
+        y = initY;
+        width = texture.getWidth()*scale;
+        height = texture.getHeight()*scale;
 
         sumDisplayBlock = new Sprite(texture);
         sumDisplayBlock.setColor(Color.TEAL);
-        sumDisplayBlock.setPosition(blockX,blockY);
-        sumDisplayBlock.setSize(blockWidth, blockHeight);
+        sumDisplayBlock.setPosition(x,y);
+        sumDisplayBlock.setSize(width, height);
     }
 
     public void draw(String sum, SpriteBatch batch){
         sumDisplayBlock.draw(batch);
 
-        float textX = blockX+blockWidth/2-sum.length()*fontSize/2f;
-        float textY = blockY+(blockHeight+fontSize)/2;
+        float textX = x+width/2-sum.length()*fontSize/2f;
+        float textY = y+(height+fontSize)/2;
 
         text.setColor(Color.WHITE);
         text.draw(batch, sum, textX, textY);
