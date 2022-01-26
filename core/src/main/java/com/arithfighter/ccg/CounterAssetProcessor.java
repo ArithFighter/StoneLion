facf7524f6c4ec832341f8cc78d16f9853932f83
catchball <palefever98@gmail.com>
2022-01-26 10:59:02 +0800

    add CounterAssetProcessor

---
+++ core/src/main/java/com/arithfighter/ccg/CounterAssetProcessor.java
@@ -0,0 +1,37 @@
+package com.arithfighter.ccg;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+
+public class CounterAssetProcessor {
+    AssetManager assetManager;
+    FileLibrary fileLibrary;
+
+    public CounterAssetProcessor(){
+        assetManager = new AssetManager();
+        fileLibrary = new FileLibrary();
+    }
+
+    public void loadAssets(){
+        for (String textureFile : fileLibrary.getTextureFile())
+            assetManager.load(textureFile, Texture.class);
+    }
+
+    public void finishLoading(){
+        assetManager.finishLoading();
+    }
+
+    public void update(int millis){
+        assetManager.update(millis);
+    }
+
+    public Texture[] getTextures() {
+        int length = fileLibrary.getTextureFile().length;
+        Texture[] textures = new Texture[length];
+
+        for (int i = 0; i < fileLibrary.getTextureFile().length; i++)
+            textures[i] = assetManager.get(fileLibrary.getTextureFile()[i]);
+
+        return textures;
+    }
+}
