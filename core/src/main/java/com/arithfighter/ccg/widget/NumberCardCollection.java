826d2726249f0c6ff9851e2434cb504e8d72e030
catchball <palefever98@gmail.com>
2022-01-26 17:23:03 +0800

    add NumberCardCollection

---
+++ core/src/main/java/com/arithfighter/ccg/widget/NumberCardCollection.java
@@ -0,0 +1,30 @@
+package com.arithfighter.ccg.widget;
+
+import com.arithfighter.ccg.WindowSetting;
+import com.badlogic.gdx.graphics.Texture;
+
+public class NumberCardCollection {
+    NumberCard minCard, midCard, maxCard, resetCard;
+    NumberCard[] cards;
+    float initX = WindowSetting.CENTER_X+WindowSetting.GRID_X*3;
+    float initY = 0;
+    float padding;
+
+    public NumberCardCollection(int min, int mid, int max, Texture texture){
+        minCard = new NumberCard(initX, initY, texture, min);
+
+        padding = minCard.getWidth()+WindowSetting.GRID_X;
+
+        midCard = new NumberCard(initX+padding, initY, texture, mid);
+
+        maxCard = new NumberCard(initX+padding*2, initY, texture, max);
+
+        resetCard = new NumberCard(initX+padding*3, initY, texture, 0);
+
+        cards = new NumberCard[]{minCard, midCard, maxCard, resetCard};
+    }
+
+    public NumberCard[] getCards(){
+        return cards;
+    }
+}
