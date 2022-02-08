461243b13e7ed1236b3b5cc83fa70f01d925c5dc
catchball <palefever98@gmail.com>
2022-02-08 14:44:11 +0800

    update SumDisplacer

--- core/src/main/java/com/arithfighter/ccg/component/SumDisplacer.java
+++ core/src/main/java/com/arithfighter/ccg/component/SumDisplacer.java
@@ -5,33 +5,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SumDisplacer {
+public class SumDisplacer extends RawWidget{
     Font text;
     Sprite sumDisplayBlock;
-    float x, y, width, height;
-    int scale = 10;
-    int fontSize = 36;
     
     public SumDisplacer(Texture texture, float initX, float initY){
+        configWidget(initX, initY, texture.getWidth(), texture.getHeight(), 10);
+        
+        configFont(36);
+        
         text = new Font(fontSize);
-
-        x = initX;
-        y = initY;
-        width = texture.getWidth()*scale;
-        height = texture.getHeight()*scale;
 
         sumDisplayBlock = new Sprite(texture);
         sumDisplayBlock.setColor(Color.GOLDENROD);
-        sumDisplayBlock.setPosition(x,y);
-        sumDisplayBlock.setSize(width, height);
+        sumDisplayBlock.setPosition(widgetX,widgetY);
+        sumDisplayBlock.setSize(widgetWidth, widgetHeight);
     }
 
     public void draw(int number, SpriteBatch batch){
         sumDisplayBlock.draw(batch);
 
         String sum = String.valueOf(number);
-        float textX = x+width/2-sum.length()*fontSize/2f;
-        float textY = y+(height+fontSize)/2;
+        float textX = widgetX+widgetWidth/2-sum.length()*fontSize/2f;
+        float textY = widgetY+(widgetHeight+fontSize)/2;
 
         text.setColor(Color.WHITE);
         text.draw(batch, sum, textX, textY);
