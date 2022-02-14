392a882976d44dfb728c327ca95a996d1d5c20bd
catchball <palefever98@gmail.com>
2022-02-14 15:30:52 +0800

    add NumberBoxDisplacer

---
+++ core/src/main/java/com/arithfighter/ccg/component/NumberBoxDisplacer.java
@@ -0,0 +1,41 @@
+package com.arithfighter.ccg.component;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import static com.arithfighter.ccg.WindowSetting.GRID_X;
+import static com.arithfighter.ccg.WindowSetting.GRID_Y;
+
+public class NumberBoxDisplacer{
+    NumberBox numberBox;
+    NumberBox[] numberBoxes;
+    NumberBoxPlacer numberBoxPlacer;
+    private final int numberBoxQuantity = 9;
+
+    public NumberBoxDisplacer(Texture[] textures){
+        numberBoxPlacer = new NumberBoxPlacer(GRID_X*9.5f,GRID_Y*5, GRID_X);
+
+        numberBox = new NumberBox(textures[3], 300, 350);
+
+
+        numberBoxes = new NumberBox[numberBoxQuantity];
+
+        for (int i = 0; i< numberBoxQuantity; i++){
+            numberBoxes[i] = new NumberBox(textures[3],
+                    numberBoxPlacer.getNumberBoxX(i, numberBox.getWidth()),
+                    numberBoxPlacer.getNumberBoxY(i, numberBox.getHeight()));
+        }
+    }
+
+    public int getNumberBoxQuantity(){
+        return numberBoxQuantity;
+    }
+
+    public NumberBox[] getNumberBoxes(){
+        return numberBoxes;
+    }
+
+    public void dispose(){
+        for (NumberBox numberBox:numberBoxes)
+            numberBox.dispose();
+    }
+}
