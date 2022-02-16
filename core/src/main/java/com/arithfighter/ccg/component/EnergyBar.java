8a53b048693ef656a70efae62d05c717eb78665b
catchball <palefever98@gmail.com>
2022-02-16 11:25:27 +0800

    add EnergyBar

---
+++ core/src/main/java/com/arithfighter/ccg/component/EnergyBar.java
@@ -0,0 +1,23 @@
+package com.arithfighter.ccg.component;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EnergyBar extends RawWidget{
+    Sprite bar;
+
+    public EnergyBar(Texture texture, float initX, float initY){
+        configWidget(initX, initY, texture.getWidth(), texture.getHeight(), 3.5f);
+
+        configFont(32);
+
+        bar = new Sprite(texture);
+        bar.setPosition(widgetX, widgetY);
+        bar.setSize(widgetWidth, widgetHeight);
+    }
+
+    public void draw(SpriteBatch batch) {
+        bar.draw(batch);
+    }
+}
