ace66428c3b9f85fa0f47d4f8c21caf81cf25aa0
catchball <palefever98@gmail.com>
2022-02-22 15:54:44 +0800

    test knight's skill card in CardHand

--- core/src/main/java/com/arithfighter/ccg/card/CardHand.java
+++ core/src/main/java/com/arithfighter/ccg/card/CardHand.java
@@ -1,27 +1,37 @@
 package com.arithfighter.ccg.card;
 
+import com.arithfighter.ccg.CharacterList;
 import com.arithfighter.ccg.WindowSetting;
 import com.badlogic.gdx.graphics.Texture;
 
-public class CardHand {
-    private final NumberCard[] cards;
+import java.util.ArrayList;
 
-    public CardHand(int[] numberSet, Texture[] textures) {
+public class CardHand {
+    private final ArrayList<NumberCard> cardList = new ArrayList<>();
+
+    public CardHand(int[] numberSet, Texture[] textures, CharacterList character) {
         float initX = WindowSetting.CENTER_X + WindowSetting.GRID_X * 1.2f;
         float initY = -WindowSetting.GRID_Y;
-        int cardQuantity = numberSet.length;
 
-        cards = new NumberCard[cardQuantity];
 
-        NumberCard sample = new NumberCard(initX, initY, textures[0], numberSet[0]);
+        for(int i = 0; i< numberSet.length;i++)
+            cardList.add(new NumberCard(initX+i*getPadding(textures), initY, textures[i], numberSet[i]));
 
-        float padding = sample.getWidth() + WindowSetting.GRID_X*0.8f;
+        if (character == CharacterList.KNIGHT){
+            NumberCard skillCard = new NumberCard(initX, initY, textures[4], 1);
+            cardList.add(skillCard);
+        }
+    }
 
-        for(int i = 0; i< cardQuantity;i++)
-            cards[i] = new NumberCard(initX+i*padding, initY, textures[i], numberSet[i]);
+    private float getPadding(Texture[] textures){
+        NumberCard sample = new NumberCard(0, 0, textures[0], 0);
+        return sample.getWidth() + WindowSetting.GRID_X*0.8f;
     }
 
     public NumberCard[] getCards() {
+        NumberCard[] cards = new NumberCard[cardList.size()];
+        for (int i = 0; i< cardList.size();i++)
+            cards[i] = cardList.get(i);
         return cards;
     }
 }
