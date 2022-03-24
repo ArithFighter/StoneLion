a68a274daa30bedd065fb27bf9e68999b1d8d989
catchball <palefever98@gmail.com>
2022-03-24 11:08:13 +0800

    update CharacterMenu

--- core/src/main/java/com/arithfighter/ccg/CharacterMenu.java
+++ core/src/main/java/com/arithfighter/ccg/CharacterMenu.java
@@ -17,65 +17,77 @@ public class CharacterMenu {
     public CharacterMenu(Texture[] textures) {
         buttons = new Button[names.length];
 
-        for (int i = 0; i<names.length;i++){
+        for (int i = 0; i < names.length; i++) {
             buttons[i] = new Button(textures[6]);
-            buttons[i].setPosition(360, 630-i*150);
+            buttons[i].setPosition(changeX(i), changeY(i));
         }
 
         startButton = new Button(textures[6]);
-        startButton.setPosition(700,200);
+        startButton.setPosition(900, 120);
 
         selectionFont = new Font(36);
         selectionFont.setColor(Color.WHITE);
     }
 
-    public void init(){
+    private int changeX(int i) {
+        return i < 3 ? 200 : 500;
+    }
+
+    private int changeY(int i) {
+        int initY = 480;
+        int margin = 150;
+        int row = 3;
+
+        return i < row ? initY - i * margin : initY - (i - row) * margin;
+    }
+
+    public void init() {
         isGameStart = false;
     }
 
     public void draw(SpriteBatch batch) {
-        for (int i = 0; i< buttons.length;i++)
+        for (int i = 0; i < buttons.length; i++)
             buttons[i].draw(batch, names[i]);
 
         startButton.draw(batch, "Start");
 
-        selectionFont.draw(batch, names[getSelectIndex()], 700,450);
+        selectionFont.draw(batch, names[getSelectIndex()], 900, 500);
     }
 
-    public int getSelectionQuantity(){
+    public int getSelectionQuantity() {
         return names.length;
     }
 
-    public int getSelectIndex(){
-        for (int i = 0; i< buttons.length;i++){
+    public int getSelectIndex() {
+        for (int i = 0; i < buttons.length; i++) {
             if (buttons[i].isActive())
                 selectIndex = i;
         }
         return selectIndex;
     }
 
-    public boolean isStart(){
+    public boolean isStart() {
         if (startButton.isActive())
             isGameStart = true;
         return isGameStart;
     }
 
     public void activateButton(int mouseX, int mouseY) {
-        for (Button button:buttons)
+        for (Button button : buttons)
             button.activate(mouseX, mouseY);
 
         startButton.activate(mouseX, mouseY);
     }
 
     public void deactivateButton() {
-        for (Button button:buttons)
+        for (Button button : buttons)
             button.deactivate();
 
         startButton.deactivate();
     }
 
-    public void dispose(){
-        for (Button button:buttons)
+    public void dispose() {
+        for (Button button : buttons)
             button.dispose();
 
         startButton.dispose();
