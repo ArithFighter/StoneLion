acc29d2de0dffdee339abafe4c5a5f072bdf06d1
catchball <palefever98@gmail.com>
2022-03-25 11:18:12 +0800

    add new skill and update SkillHandler

--- core/src/main/java/com/arithfighter/ccg/SkillHandler.java
+++ core/src/main/java/com/arithfighter/ccg/SkillHandler.java
@@ -3,8 +3,11 @@ package com.arithfighter.ccg;
 import com.arithfighter.ccg.entity.CharacterList;
 import com.arithfighter.ccg.entity.NumberBoxDisplacer;
 
+import java.util.ArrayList;
+
 public class SkillHandler {
     NumberBoxDisplacer numberBoxDisplacer;
+
     public SkillHandler(NumberBoxDisplacer numberBoxDisplacer){
         this.numberBoxDisplacer = numberBoxDisplacer;
     }
@@ -12,26 +15,44 @@ public class SkillHandler {
     public void cast(CharacterList character){
         switch (character) {
             case KNIGHT:
-                //change one value of numberBox
-                numberBoxDisplacer.set(0, 33);
-                break;
-            case ROGUE:
                 reduceAllValueByOne();
                 break;
+            case ROGUE:
+                changeANoneZeroValueToSixteen();
+                break;
         }
+    }
+
+    private void reduceAllValueByOne(){
+        for (int i = 0; i < numberBoxDisplacer.getNumberBoxQuantity(); i++){
+
+            if (isValueBiggerThanZero(i))
+                numberBoxDisplacer.set(i, getNumberBoxValue(i) - 1);
+        }
+    }
+
+    private void changeANoneZeroValueToSixteen(){
+        ArrayList<Integer> indexList = new ArrayList<>();
+
+        for (int i = 0; i < numberBoxDisplacer.getNumberBoxQuantity(); i++){
+            if (isValueBiggerThanZero(i))
+                indexList.add(i);
+        }
+
+        int indexPick = getRandomNum(indexList.size());
+
+        numberBoxDisplacer.set(indexList.get(indexPick), 16);
+    }
+
+    private boolean isValueBiggerThanZero(int i){
+        return getNumberBoxValue(i) > 0;
     }
 
     private int getNumberBoxValue(int i){
         return numberBoxDisplacer.getNumberList().get(i);
     }
 
-    private void reduceAllValueByOne(){
-        int quantity = numberBoxDisplacer.getNumberBoxQuantity();
-        for (int i = 0; i < quantity; i++)
-                    if (getNumberBoxValue(i) > 0) {
-                        numberBoxDisplacer.set(
-                                i,
-                                getNumberBoxValue(i) - 1);
-                    }
+    private int getRandomNum(int max){
+        return (int)(Math.random() * (max + 1));
     }
 }
