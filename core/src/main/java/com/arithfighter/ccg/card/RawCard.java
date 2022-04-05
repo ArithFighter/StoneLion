5fd9d1eb647b2ef8f0452346b6a94815b953881d
catchball <palefever98@gmail.com>
2022-04-05 12:02:03 +0800

    refactor RawCard

--- core/src/main/java/com/arithfighter/ccg/card/RawCard.java
+++ core/src/main/java/com/arithfighter/ccg/card/RawCard.java
@@ -4,28 +4,14 @@ import com.arithfighter.ccg.pojo.Point;
 import com.arithfighter.ccg.pojo.Shape;
 
 public class RawCard {
-    private Point initPoint;
-    private Point point;
+    private final Point initPoint;
+    private final Point point;
     private Shape shape;
-
-    public RawCard(){
-        shape = new Shape();
-        initPoint = new Point(0,0);
-        point = new Point(0,0);
-    }
 
     public RawCard(float initX, float initY){
         shape = new Shape();
         initPoint = new Point(initX,initY);
         point = new Point(initX,initY);
-    }
-
-    public void setInitPoint(Point initPoint) {
-        this.initPoint = initPoint;
-    }
-
-    public void setPoint(Point point) {
-        this.point = point;
     }
 
     public Point getInitPoint() {
