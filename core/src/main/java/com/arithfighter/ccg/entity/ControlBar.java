6d59085f3b434d9185a8ee5c3b4360b6ff2b716f
catchball <palefever98@gmail.com>
2022-04-07 11:16:01 +0800

    add ButtonProducer to ControlBar

--- core/src/main/java/com/arithfighter/ccg/entity/ControlBar.java
+++ core/src/main/java/com/arithfighter/ccg/entity/ControlBar.java
@@ -12,23 +12,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class ControlBar {
     private final VisibleWidget[] grids;
     private final Font font;
-    private final Button leftArrow;
-    private final Button rightArrow;
+    private final ButtonProducer arrows;
     private final int maxValue = 6;
     private int value;
     private final int fontSize;
-    private boolean buttonLock = true;
+    private boolean isButtonLock = false;
 
     public ControlBar(Texture[] textures){
         grids = new VisibleWidget[maxValue];
-        for (int i = 0; i< grids.length;i++){
+
+        for (int i = 0; i< grids.length;i++)
             grids[i] = new SpriteWidget(textures[5], 0.5f);
-        }
+
+        arrows = new ButtonProducer(textures);
+
         value = maxValue;
-
-        leftArrow = new Button(textures[8], 0.8f);
-
-        rightArrow = new Button(textures[9], 0.8f);
 
         fontSize = 25;
         font = new Font(fontSize);
@@ -48,27 +46,73 @@ public class ControlBar {
         for (int i =0;i<value;i++)
             grids[i].draw(batch);
 
+        addFont(batch, content);
+
+        Widget widget = grids[0].getWidget();
+        Point point = widget.getPoint();
+
+        arrows.drawLeftArrow(batch, point.getX()-50, point.getY());
+        arrows.drawRightArrow(batch, point.getX(), point.getY(), (widget.getWidth()+10) * grids.length);
+    }
+
+    private void addFont(SpriteBatch batch, String content){
         Widget widget = grids[0].getWidget();
         Point point = widget.getPoint();
         font.draw(batch, content, point.getX()-content.length()*fontSize-50, point.getY()+fontSize/2f);
-
-        leftArrow.setPosition(point.getX()-50, point.getY());
-        leftArrow.draw(batch, "");
-
-        rightArrow.setPosition(point.getX()+(widget.getWidth()+10) * grids.length, point.getY());
-        rightArrow.draw(batch,"");
     }
 
     public void update(){
-        if (value<=0)
-            value = 0;
-        if (buttonLock){
-            if (leftArrow.isActive() && value>=0)
+        value = Math.max(0,value);
+
+        if (!isButtonLock){
+            if (arrows.isLeftActive() && value>=0)
                 value-=1;
-            if (rightArrow.isActive() && value< maxValue)
+            if (arrows.isRightActive() && value< maxValue)
                 value+=1;
         }
-        buttonLock = !leftArrow.isActive() && !rightArrow.isActive();
+        isButtonLock = arrows.isLeftActive() || arrows.isRightActive();
+    }
+
+    public void activate(float x, float y){
+        arrows.activate(x, y);
+    }
+
+    public void deactivate(){
+        arrows.deactivate();
+    }
+
+    public void dispose(){
+        font.dispose();
+        arrows.dispose();
+    }
+}
+
+class ButtonProducer{
+    private final Button leftArrow;
+    private final Button rightArrow;
+
+    public ButtonProducer(Texture[] textures){
+        leftArrow = new Button(textures[8], 0.8f);
+
+        rightArrow = new Button(textures[9], 0.8f);
+    }
+
+    public void drawLeftArrow(SpriteBatch batch, float x, float y){
+        leftArrow.setPosition(x-50, y);
+        leftArrow.draw(batch, "");
+    }
+
+    public void drawRightArrow(SpriteBatch batch, float x, float y, float barWidth){
+        rightArrow.setPosition(x+barWidth, y);
+        rightArrow.draw(batch,"");
+    }
+
+    public boolean isLeftActive(){
+        return leftArrow.isActive();
+    }
+
+    public boolean isRightActive(){
+        return rightArrow.isActive();
     }
 
     public void activate(float x, float y){
@@ -82,7 +126,6 @@ public class ControlBar {
     }
 
     public void dispose(){
-        font.dispose();
         leftArrow.dispose();
         rightArrow.dispose();
     }
