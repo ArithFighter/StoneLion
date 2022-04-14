8137a963970227cb2bc3a70b6d1906562544c4e4
catchball <palefever98@gmail.com>
2022-04-14 12:11:58 +0800

    add PlayerCollection to entity

---
+++ core/src/main/java/com/arithfighter/not/entity/PlayerCollection.java
@@ -0,0 +1,134 @@
+package com.arithfighter.not.entity;
+
+import com.arithfighter.not.entity.player.CharacterList;
+import com.arithfighter.not.entity.player.Player;
+import com.arithfighter.not.pojo.GameNumProducer;
+import com.arithfighter.not.pojo.Recorder;
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
+
+public class PlayerCollection {
+    private final Player[] players;
+    private Recorder playRecord;
+    private NumberBoxDisplacer numberBoxDisplacer;
+    private final int characterQuantity;
+
+    public PlayerCollection(Texture[] textures, Texture[] cards, int characterQuantity) {
+        this.characterQuantity = characterQuantity;
+        players = new Player[characterQuantity];
+        createPlayers(textures, cards);
+    }
+
+    public void setPlayRecord(Recorder playRecord) {
+        this.playRecord = playRecord;
+    }
+
+    public void setNumberBoxDisplacer(NumberBoxDisplacer numberBoxDisplacer) {
+        this.numberBoxDisplacer = numberBoxDisplacer;
+    }
+
+    private void createPlayers(Texture[] textures, Texture[] cards) {
+        SkillHandler skillHandler = new SkillHandler(numberBoxDisplacer);
+
+        for (int i = 0; i < characterQuantity; i++)
+            players[i] = new Player(
+                    textures,
+                    cards,
+                    CharacterList.values()[i]) {
+                @Override
+                public void doWhenCardPlayed() {
+                    playRecord.update(1);
+                }
+
+                @Override
+                public void castSkill(CharacterList character) {
+                    skillHandler.cast(character);
+                }
+            };
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void dispose() {
+        for (Player player : players) player.dispose();
+    }
+}
+
+class SkillHandler {
+    private final NumberBoxDisplacer numberBoxDisplacer;
+    private final IndexPicker indexPicker;
+
+    public SkillHandler(NumberBoxDisplacer numberBoxDisplacer) {
+        this.numberBoxDisplacer = numberBoxDisplacer;
+        indexPicker = new IndexPicker(numberBoxDisplacer);
+    }
+
+    public void cast(CharacterList character) {
+        switch (character) {
+            case KNIGHT:
+                replaceOneNonZeroValue(9);
+                break;
+            case ROGUE:
+                replaceOneNonZeroValue(8);
+                break;
+            case HUNTER:
+                replaceOneNonZeroValue(14);
+                break;
+            case PALADIN:
+                replaceOneNonZeroValue(22);
+                break;
+            case WARRIOR:
+                increaseAllValueByFour();
+                break;
+        }
+    }
+
+    private void increaseAllValueByFour() {
+        int gain = 4;
+
+        for (int i = 0; i < numberBoxDisplacer.getMaxQuantity(); i++) {
+            if (getNumberBoxValue(i) > 0 &&
+                    getNumberBoxValue(i) < new GameNumProducer().getMax() - gain)
+                numberBoxDisplacer.set(i, getNumberBoxValue(i) + gain);
+        }
+    }
+
+    private void replaceOneNonZeroValue(int value) {
+        int index = indexPicker.getRandomNonZeroValueIndex();
+
+        numberBoxDisplacer.set(index, value);
+    }
+
+    private int getNumberBoxValue(int i) {
+        return numberBoxDisplacer.getNumberBoxValue(i);
+    }
+}
+
+class IndexPicker {
+    private final NumberBoxDisplacer numberBoxDisplacer;
+
+    public IndexPicker(NumberBoxDisplacer numberBoxDisplacer) {
+        this.numberBoxDisplacer = numberBoxDisplacer;
+    }
+
+    public int getRandomNonZeroValueIndex() {
+        ArrayList<Integer> indexList = new ArrayList<>();
+
+        for (int i = 0; i < numberBoxDisplacer.getMaxQuantity(); i++) {
+            if (numberBoxDisplacer.getNumberBoxValue(i) > 0)
+                indexList.add(i);
+        }
+
+        int indexPick = getRandomNum(indexList.size() - 1);
+
+        return indexList.get(indexPick);
+    }
+
+
+    private int getRandomNum(int range) {
+        return (int) (Math.random() * (range + 1));
+    }
+}
--- core/src/main/java/com/arithfighter/not/scene/Stage.java
+++ core/src/main/java/com/arithfighter/not/scene/Stage.java
@@ -3,15 +3,11 @@ package com.arithfighter.not.scene;
 import com.arithfighter.not.audio.SoundManager;
 import com.arithfighter.not.entity.*;
 import com.arithfighter.not.entity.player.CharacterList;
-import com.arithfighter.not.entity.player.Player;
 import com.arithfighter.not.CursorPositionAccessor;
-import com.arithfighter.not.pojo.GameNumProducer;
 import com.arithfighter.not.pojo.Recorder;
 import com.arithfighter.not.pojo.TokenHolder;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import java.util.ArrayList;
 
 public class Stage implements SceneEvent, MouseEvent{
     private final PlayerCollection playerCollection;
@@ -125,130 +121,5 @@ public class Stage implements SceneEvent, MouseEvent{
         gamePlayComponent.dispose();
         pauseMenu.dispose();
         playerCollection.dispose();
-    }
-}
-
-class PlayerCollection {
-    private final Player[] players;
-    private Recorder playRecord;
-    private NumberBoxDisplacer numberBoxDisplacer;
-    private final int characterQuantity;
-
-    public PlayerCollection(Texture[] textures, Texture[] cards, int characterQuantity){
-        this.characterQuantity = characterQuantity;
-        players = new Player[characterQuantity];
-        createPlayers(textures, cards);
-    }
-
-    public void setPlayRecord(Recorder playRecord) {
-        this.playRecord = playRecord;
-    }
-
-    public void setNumberBoxDisplacer(NumberBoxDisplacer numberBoxDisplacer) {
-        this.numberBoxDisplacer = numberBoxDisplacer;
-    }
-
-    private void createPlayers(Texture[] textures, Texture[] cards) {
-        SkillHandler skillHandler = new SkillHandler(numberBoxDisplacer);
-
-        for (int i = 0; i < characterQuantity; i++)
-            players[i] = new Player(
-                    textures,
-                    cards,
-                    CharacterList.values()[i]) {
-                @Override
-                public void doWhenCardPlayed() {
-                    playRecord.update(1);
-                }
-
-                @Override
-                public void castSkill(CharacterList character) {
-                    skillHandler.cast(character);
-                }
-            };
-    }
-
-    public Player[] getPlayers(){
-        return players;
-    }
-
-    public void dispose(){
-        for (Player player : players) player.dispose();
-    }
-}
-
-class SkillHandler {
-    private final NumberBoxDisplacer numberBoxDisplacer;
-    private final IndexPicker indexPicker;
-
-    public SkillHandler(NumberBoxDisplacer numberBoxDisplacer) {
-        this.numberBoxDisplacer = numberBoxDisplacer;
-        indexPicker = new IndexPicker(numberBoxDisplacer);
-    }
-
-    public void cast(CharacterList character) {
-        switch (character) {
-            case KNIGHT:
-                replaceOneNonZeroValue(9);
-                break;
-            case ROGUE:
-                replaceOneNonZeroValue(8);
-                break;
-            case HUNTER:
-                replaceOneNonZeroValue(14);
-                break;
-            case PALADIN:
-                replaceOneNonZeroValue(22);
-                break;
-            case WARRIOR:
-                increaseAllValueByFour();
-                break;
-        }
-    }
-
-    private void increaseAllValueByFour() {
-        int gain = 4;
-
-        for (int i = 0; i < numberBoxDisplacer.getMaxQuantity(); i++) {
-            if (getNumberBoxValue(i) > 0 &&
-                    getNumberBoxValue(i) < new GameNumProducer().getMax()-gain)
-                numberBoxDisplacer.set(i, getNumberBoxValue(i) + gain);
-        }
-    }
-
-    private void replaceOneNonZeroValue(int value) {
-        int index = indexPicker.getRandomNonZeroValueIndex();
-
-        numberBoxDisplacer.set(index, value);
-    }
-
-    private int getNumberBoxValue(int i) {
-        return numberBoxDisplacer.getNumberBoxValue(i);
-    }
-}
-
-class IndexPicker {
-    private final NumberBoxDisplacer numberBoxDisplacer;
-
-    public IndexPicker(NumberBoxDisplacer numberBoxDisplacer) {
-        this.numberBoxDisplacer = numberBoxDisplacer;
-    }
-
-    public int getRandomNonZeroValueIndex() {
-        ArrayList<Integer> indexList = new ArrayList<>();
-
-        for (int i = 0; i < numberBoxDisplacer.getMaxQuantity(); i++) {
-            if (numberBoxDisplacer.getNumberBoxValue(i) > 0)
-                indexList.add(i);
-        }
-
-        int indexPick = getRandomNum(indexList.size()-1);
-
-        return indexList.get(indexPick);
-    }
-
-
-    private int getRandomNum(int range) {
-        return (int) (Math.random() * (range + 1));
     }
 }
