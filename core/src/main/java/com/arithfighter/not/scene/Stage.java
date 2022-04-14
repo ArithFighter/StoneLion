6248e75e6594349a18a3dd41c487e7c4131e4ec7
catchball <palefever98@gmail.com>
2022-04-14 12:02:31 +0800

    add Players to Stage

--- core/src/main/java/com/arithfighter/not/scene/Stage.java
+++ core/src/main/java/com/arithfighter/not/scene/Stage.java
@@ -14,13 +14,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.util.ArrayList;
 
 public class Stage implements SceneEvent, MouseEvent{
-    private final Player[] players;
+    private final Players players;
     private final GamePlayComponent gamePlayComponent;
     private final PauseMenu pauseMenu;
     private final GameDataDisplacer dataDisplacer;
     private CursorPositionAccessor cursorPos;
     private final TokenHolder tokenHolder;
-    private final int characterQuantity = CharacterList.values().length;
     private SpriteBatch batch;
     private final Recorder playRecord = new Recorder();
     private final int initTokens = 10;
@@ -29,35 +28,16 @@ public class Stage implements SceneEvent, MouseEvent{
     public Stage(Texture[] textures, Texture[] cards, SoundManager soundManager) {
         dataDisplacer = new GameDataDisplacer();
 
-        players = new Player[characterQuantity];
-
         gamePlayComponent = new GamePlayComponent(textures, soundManager);
 
         pauseMenu = new PauseMenu(textures, soundManager);
 
         tokenHolder = new TokenHolder(initTokens);
 
-        createPlayers(textures, cards);
-    }
-
-    private void createPlayers(Texture[] textures, Texture[] cards) {
-        SkillHandler skillHandler = new SkillHandler(gamePlayComponent.getNumberBoxDisplacer());
-
-        for (int i = 0; i < characterQuantity; i++)
-            players[i] = new Player(
-                    textures,
-                    cards,
-                    CharacterList.values()[i]) {
-                @Override
-                public void doWhenCardPlayed() {
-                    playRecord.update(1);
-                }
-
-                @Override
-                public void castSkill(CharacterList character) {
-                    skillHandler.cast(character);
-                }
-            };
+        players = new Players(textures, cards);
+        players.setCharacterQuantity(CharacterList.values().length);
+        players.setPlayRecord(playRecord);
+        players.setNumberBoxDisplacer(gamePlayComponent.getNumberBoxDisplacer());
     }
 
     public boolean isExceedCardLimit(int limit){
@@ -113,14 +93,14 @@ public class Stage implements SceneEvent, MouseEvent{
         //for dev
         dataDisplacer.setCardPlayTimes(playRecord.getRecord());
         dataDisplacer.setScore(gamePlayComponent.getScore());
-        dataDisplacer.setEnergy(players[index].getEnergy());
+        dataDisplacer.setEnergy(players.getPlayers()[index].getEnergy());
         dataDisplacer.setCursorPoint(cursorPos.getX(), cursorPos.getY());
         dataDisplacer.setToken(tokenHolder.getToken());
         dataDisplacer.draw(batch);
     }
 
     public void setSelectedPlayerToGame(int i) {
-        gamePlayComponent.setPlayer(players[i]);
+        gamePlayComponent.setPlayer(players.getPlayers()[i]);
     }
 
     public void touchDown() {
@@ -145,6 +125,58 @@ public class Stage implements SceneEvent, MouseEvent{
         dataDisplacer.dispose();
         gamePlayComponent.dispose();
         pauseMenu.dispose();
+        players.dispose();
+    }
+}
+
+class Players{
+    private final Player[] players;
+    private Recorder playRecord;
+    private NumberBoxDisplacer numberBoxDisplacer;
+    private int characterQuantity;
+
+    public Players(Texture[] textures, Texture[] cards){
+        players = new Player[characterQuantity];
+        createPlayers(textures, cards);
+    }
+
+    public void setPlayRecord(Recorder playRecord) {
+        this.playRecord = playRecord;
+    }
+
+    public void setNumberBoxDisplacer(NumberBoxDisplacer numberBoxDisplacer) {
+        this.numberBoxDisplacer = numberBoxDisplacer;
+    }
+
+    public void setCharacterQuantity(int characterQuantity) {
+        this.characterQuantity = characterQuantity;
+    }
+
+    private void createPlayers(Texture[] textures, Texture[] cards) {
+        SkillHandler skillHandler = new SkillHandler(numberBoxDisplacer);
+
+        for (int i = 0; i < characterQuantity; i++)
+            players[i] = new Player(
+                    textures,
+                    cards,
+                    CharacterList.values()[i]) {
+                @Override
+                public void doWhenCardPlayed() {
+                    playRecord.update(1);
+                }
+
+                @Override
+                public void castSkill(CharacterList character) {
+                    skillHandler.cast(character);
+                }
+            };
+    }
+
+    public Player[] getPlayers(){
+        return players;
+    }
+
+    public void dispose(){
         for (Player player : players) player.dispose();
     }
 }
