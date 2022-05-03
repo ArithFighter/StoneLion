0e534dc5eb4720d4fd86239f7d839a33913971c7
catchball <palefever98@gmail.com>
2022-05-03 16:17:57 +0800

    update GameSave

--- core/src/main/java/com/arithfighter/not/GameSave.java
+++ core/src/main/java/com/arithfighter/not/GameSave.java
@@ -9,13 +9,33 @@ public class GameSave {
             "sound volume",
             "music volume"
     };
+    private final String[] highScoreKey = {
+            "knight best",
+            "ninja best",
+            "hunter best",
+            "paladin best",
+            "warrior best"
+    };
 
     public GameSave(){
         preferences = Gdx.app.getPreferences("Numbers-on-Table-data");
 
+        initOptionSave();
+
+        initHighScore();
+    }
+
+    private void initOptionSave(){
         for (String s:optionKeys){
             if (!preferences.contains(s))
                 preferences.putInteger(s, 3);
+        }
+    }
+
+    private void initHighScore(){
+        for (String s:highScoreKey){
+            if (!preferences.contains(s))
+                preferences.putInteger(s, 99999);
         }
     }
 
@@ -25,5 +45,9 @@ public class GameSave {
 
     public String[] getOptionKeys(){
         return optionKeys;
+    }
+
+    public String[] getHighScoreKey() {
+        return highScoreKey;
     }
 }
