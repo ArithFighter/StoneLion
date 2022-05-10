d7264f012f8ef8fa91340be433f7d87ece2d6625
catchball <palefever98@gmail.com>
2022-05-10 16:50:12 +0800

    update gecko spriteSheets in assets

--- core/src/main/java/com/arithfighter/not/widget/stagecomponent/SumBox.java
+++ core/src/main/java/com/arithfighter/not/widget/stagecomponent/SumBox.java
@@ -7,21 +7,21 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SumBox{
+public class SumBox {
     private final Font text;
     private final FontWidget widget;
     private int capacity;
 
-    public SumBox(Texture texture){
-        widget = new SpriteWidget(texture,8, 36);
+    public SumBox(Texture texture) {
+        widget = new SpriteWidget(texture, 8, 36);
         text = new Font(widget.getFontSize());
     }
 
-    public void setPosition(float x, float y){
-        widget.setPosition(x,y);
+    public void setPosition(float x, float y) {
+        widget.setPosition(x, y);
     }
 
-    public void draw(int number, SpriteBatch batch){
+    public void draw(int number, SpriteBatch batch) {
         widget.draw(batch);
 
         changeColor();
@@ -29,7 +29,7 @@ public class SumBox{
         drawText(number, batch);
     }
 
-    private void drawText(int number, SpriteBatch batch){
+    private void drawText(int number, SpriteBatch batch) {
         String sum = String.valueOf(number);
 
         float textX = widget.getCenterX(sum);
@@ -43,19 +43,19 @@ public class SumBox{
         this.capacity = capacity;
     }
 
-    private void changeColor(){
+    private void changeColor() {
         if (isCapacityWarning())
             widget.getSprite().setColor(Color.RED);
         else
             widget.getSprite().setColor(Color.TEAL);
     }
 
-    public boolean isCapacityWarning(){
+    public boolean isCapacityWarning() {
         int capacityWarning = 2;
-        return capacity<= capacityWarning;
+        return capacity <= capacityWarning;
     }
 
-    public void dispose(){
+    public void dispose() {
         text.dispose();
     }
 }
