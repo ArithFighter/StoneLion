2eba6094c0e7041aa6895c4d33f700f5f19e5755
catchball <palefever98@gmail.com>
2022-05-25 11:23:15 +0800

    update RecordDisplacer

--- core/src/main/java/com/arithfighter/not/entity/RecordDisplacer.java
+++ core/src/main/java/com/arithfighter/not/entity/RecordDisplacer.java
@@ -9,19 +9,16 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class RecordDisplacer {
-    private final Font[] texts;
+    private final Font font;
     private final VisibleWidget background;
     private GameRecorder gameRecorder;
     private final int posX;
 
     public RecordDisplacer(Texture[] textures) {
-        texts = new Font[4];
         posX = 0;
 
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = new Font(20);
-            texts[i].setColor(Color.BLACK);
-        }
+        font= new Font(20);
+        font.setColor(Color.BLACK);
 
         background = new SpriteWidget(textures[1], 6f);
         background.setPosition(posX, 150);
@@ -34,14 +31,13 @@ public class RecordDisplacer {
     public void draw(SpriteBatch batch) {
         background.draw(batch);
         int x = posX + 50;
-        texts[0].draw(batch, "stage: " + gameRecorder.getStagesRecorder().getRecord(), x, 700);
-        texts[1].draw(batch, "win: " + gameRecorder.getWinRecorder().getRecord(), x, 600);
-        texts[2].draw(batch, "lose: " + gameRecorder.getLoseRecorder().getRecord(), x, 500);
-        texts[3].draw(batch, "tokens: " + gameRecorder.getTokenRecorder().getRecord(), x, 400);
+        font.draw(batch, "stage: " + gameRecorder.getStagesRecorder().getRecord(), x, 700);
+        font.draw(batch, "win: " + gameRecorder.getWinRecorder().getRecord(), x, 600);
+        font.draw(batch, "lose: " + gameRecorder.getLoseRecorder().getRecord(), x, 500);
+        font.draw(batch, "tokens: " + gameRecorder.getTokenRecorder().getRecord(), x, 400);
     }
 
     public void dispose() {
-        for (Font font : texts)
-            font.dispose();
+        font.dispose();
     }
 }
