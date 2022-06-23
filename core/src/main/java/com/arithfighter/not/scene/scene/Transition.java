17e88abe17d28c598ee4ed1c33c4c828a43be0bb
catchball <palefever98@gmail.com>
2022-06-23 10:40:21 +0800

    update Transition

--- core/src/main/java/com/arithfighter/not/scene/scene/Transition.java
+++ core/src/main/java/com/arithfighter/not/scene/scene/Transition.java
@@ -1,10 +1,28 @@
 package com.arithfighter.not.scene.scene;
 
+import com.arithfighter.not.TextureService;
+import com.arithfighter.not.WindowSetting;
+import com.arithfighter.not.font.Font;
+import com.arithfighter.not.font.FontService;
 import com.arithfighter.not.scene.MouseEvent;
 import com.arithfighter.not.scene.SceneComponent;
 import com.arithfighter.not.scene.SceneEvent;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Transition extends SceneComponent implements SceneEvent, MouseEvent {
+    private final Font font;
+    private boolean isGameStart = false;
+
+    public Transition(TextureService textureService, FontService fontService){
+        font = fontService.getFont32();
+        font.setColor(Color.WHITE);
+    }
+
+    public boolean isGameStart(){
+        return isGameStart;
+    }
+
     @Override
     public void touchDown() {
 
@@ -27,6 +45,14 @@ public class Transition extends SceneComponent implements SceneEvent, MouseEvent
 
     @Override
     public void render() {
+        SpriteBatch batch = getBatch();
+        String ready = "Ready";
 
+        font.draw(
+                batch,
+                ready,
+                WindowSetting.CENTER_X-ready.length()* font.getSize(),
+                WindowSetting.CENTER_Y- font.getSize()/2f
+        );
     }
 }
