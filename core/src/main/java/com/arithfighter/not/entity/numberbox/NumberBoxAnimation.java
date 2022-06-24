6327129ae51dec9feef905850d1ade26dee983cf
catchball <palefever98@gmail.com>
2022-06-24 11:56:10 +0800

    add Timer to NumberBoxAnimation and update

--- core/src/main/java/com/arithfighter/not/entity/numberbox/NumberBoxAnimation.java
+++ core/src/main/java/com/arithfighter/not/entity/numberbox/NumberBoxAnimation.java
@@ -2,11 +2,12 @@ package com.arithfighter.not.entity.numberbox;
 
 import com.arithfighter.not.animate.VisualEffect;
 import com.arithfighter.not.time.TimeHandler;
+import com.arithfighter.not.time.Timer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 class NumberBoxAnimation {
     private final VisualEffect visualEffect;
-    private final TimeHandler timeHandler;
+    private final Timer timer;
     private SpriteBatch batch;
     private int matchedBoxIndex = -1;
 
@@ -17,7 +18,8 @@ class NumberBoxAnimation {
                 numberBoxes[matchedBoxIndex].draw(batch, 0);
             }
         };
-        timeHandler = new TimeHandler();
+        timer = new Timer();
+        timer.setTime(1.2f);
     }
 
     public void setBatch(SpriteBatch batch) {
@@ -30,20 +32,19 @@ class NumberBoxAnimation {
 
     public void draw() {
         int ratePerSec = 8;
-        float durationSec = 1.2f;
 
         if (matchedBoxIndex >= 0) {
-            timeHandler.updatePastedTime();
+            timer.update();
 
             visualEffect.animateFlashy(ratePerSec);
 
-            if (timeHandler.getPastedTime() > durationSec)
+            if (timer.isTimesOut())
                 init();
         }
     }
 
     public void init() {
-        timeHandler.resetPastedTime();
+        timer.init();
         matchedBoxIndex -= matchedBoxIndex + 1;
     }
 }
