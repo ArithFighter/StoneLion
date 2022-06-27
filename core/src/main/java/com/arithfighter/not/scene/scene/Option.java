a01e0f2992398a151f7933d6688f5b1edced2b39
catchball <palefever98@gmail.com>
2022-06-27 11:43:44 +0800

    update Option

--- core/src/main/java/com/arithfighter/not/scene/scene/Option.java
+++ core/src/main/java/com/arithfighter/not/scene/scene/Option.java
@@ -1,37 +1,88 @@
 package com.arithfighter.not.scene.scene;
 
+import com.arithfighter.not.CursorPositionAccessor;
+import com.arithfighter.not.TextureService;
+import com.arithfighter.not.audio.SoundManager;
+import com.arithfighter.not.font.Font;
+import com.arithfighter.not.font.FontService;
+import com.arithfighter.not.pojo.TextProvider;
+import com.arithfighter.not.scene.GameScene;
 import com.arithfighter.not.scene.MouseEvent;
 import com.arithfighter.not.scene.SceneComponent;
 import com.arithfighter.not.scene.SceneEvent;
+import com.arithfighter.not.widget.a1.ControlBar;
+import com.arithfighter.not.widget.button.SceneControlButton;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Option extends SceneComponent implements SceneEvent, MouseEvent {
+    private final SceneControlButton leaveButton;
+    private final SoundManager soundManager;
+    private final ControlBar soundControl;
+    private final ControlBar musicControl;
+    private GameScene sceneTemp;
 
-    public Option(){
+    public Option(TextureService textureService, SoundManager soundManager, FontService fontService){
+        this.soundManager = soundManager;
+        Texture[] textures = textureService.getTextures(textureService.getKeys()[0]);
+        Font font = fontService.getFont22();
 
+        leaveButton = new SceneControlButton(textures[6], 1.8f);
+        leaveButton.getButton().setFont(font);
+        leaveButton.getButton().setPosition(500,120);
+
+        soundControl = new ControlBar(textures, 6);
+        soundControl.setFont(font);
+        soundControl.setPosition(500,600);
+
+        musicControl = new ControlBar(textures, 6);
+        musicControl.setFont(font);
+        musicControl.setPosition(500,400);
     }
 
     @Override
     public void touchDown() {
+        CursorPositionAccessor cursorPos = getCursorPos();
+        int x= cursorPos.getX();
+        int y = cursorPos.getY();
 
+        soundControl.activate(x, y);
+        musicControl.activate(x, y);
+        leaveButton.getButton().on(x, y);
     }
 
     @Override
     public void touchDragged() {
-
+        soundControl.deactivate();
+        musicControl.deactivate();
+        leaveButton.getButton().off();
     }
 
     @Override
     public void touchUp() {
+        if (soundControl.isButtonActive()|| musicControl.isButtonActive())
+            soundManager.playTouchedSound();
 
+        if (leaveButton.getButton().isOn())
+            soundManager.playReturnSound();
+
+        soundControl.deactivate();
+        musicControl.deactivate();
+        leaveButton.getButton().off();
     }
 
     @Override
     public void init() {
-
+        leaveButton.init();
     }
 
     @Override
     public void render() {
+        leaveButton.update();
+        soundControl.update();
+        musicControl.update();
 
+        leaveButton.getButton().draw(getBatch(), "Quit");
+        soundControl.draw(getBatch(), "SOUND");
+        musicControl.draw(getBatch(), "MUSIC");
     }
 }
