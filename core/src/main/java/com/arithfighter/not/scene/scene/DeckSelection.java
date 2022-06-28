9d2f607a2fd03266f93457d76a8f373e6c4c1eea
catchball <palefever98@gmail.com>
2022-06-28 10:15:41 +0800

    add DeckSelection to scene

---
+++ core/src/main/java/com/arithfighter/not/scene/scene/DeckSelection.java
@@ -0,0 +1,31 @@
+package com.arithfighter.not.scene.scene;
+
+import com.arithfighter.not.scene.MouseEvent;
+import com.arithfighter.not.scene.SceneEvent;
+
+public class DeckSelection extends SceneComponent implements SceneEvent, MouseEvent {
+    @Override
+    public void touchDown() {
+
+    }
+
+    @Override
+    public void touchDragged() {
+
+    }
+
+    @Override
+    public void touchUp() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
