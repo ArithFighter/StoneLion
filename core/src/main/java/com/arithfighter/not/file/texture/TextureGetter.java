a461733755de39e7143c38eb581d181c462fcd51
catchball <palefever98@gmail.com>
2022-07-12 14:26:03 +0800

    update TextureGetter

--- core/src/main/java/com/arithfighter/not/file/texture/TextureGetter.java
+++ core/src/main/java/com/arithfighter/not/file/texture/TextureGetter.java
@@ -8,21 +8,40 @@ import java.util.Map;
 
 public class TextureGetter {
     private final TextureService textureService;
-    private final TextureMapProducer guiMapProducer;
+    private final TextureMapProducer[] textureMapProducers;
 
     public TextureGetter(TextureService textureService) {
         this.textureService = textureService;
 
         AssetNameLibrary assetNameLibrary = new AssetNameLibrary();
 
-        guiMapProducer = new TextureMapProducer(
-                assetNameLibrary.getTexturePathCollection()[0],
-                textureService.getTextureMap().get(textureService.getKeys()[0])
-        );
+        textureMapProducers = new TextureMapProducer[textureService.getKeys().length];
+
+        for(int i =0; i< textureMapProducers.length;i++)
+            textureMapProducers[i] = new TextureMapProducer(
+                    assetNameLibrary.getTexturePathCollection()[i],
+                textureService.getTextureMap().get(textureService.getKeys()[i])
+            );
     }
 
     public Map<String, Texture> getGuiMap() {
-        return guiMapProducer.getTextureMap();
+        return textureMapProducers[0].getTextureMap();
+    }
+
+    public Map<String, Texture> getCardMap(){
+        return textureMapProducers[1].getTextureMap();
+    }
+
+    public Map<String, Texture> getPanelMap(){
+        return textureMapProducers[2].getTextureMap();
+    }
+
+    public Map<String, Texture> getAnimationMap(){
+        return textureMapProducers[3].getTextureMap();
+    }
+
+    public Map<String, Texture> getObjectMap(){
+        return textureMapProducers[4].getTextureMap();
     }
 
     public Texture[] getGUIs(){
