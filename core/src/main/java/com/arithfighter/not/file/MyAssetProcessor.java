10feaf6421d5c183a82d1bdefcd3d2412cc35d9c
catchball <palefever98@gmail.com>
2022-07-12 11:40:48 +0800

    add MyAssetLoader to MyAssetProcessor

--- core/src/main/java/com/arithfighter/not/file/MyAssetProcessor.java
+++ core/src/main/java/com/arithfighter/not/file/MyAssetProcessor.java
@@ -8,26 +8,30 @@ import com.badlogic.gdx.graphics.Texture;
 public class MyAssetProcessor {
     private final AssetManager assetManager;
     private final AssetNameLibrary assetNameLibrary;
+    private final String[][] textureCollection;
 
     public MyAssetProcessor(){
         assetManager = new AssetManager();
         assetNameLibrary = new AssetNameLibrary();
+
+        textureCollection = new String[][]{
+                assetNameLibrary.getGuiFiles(),
+                assetNameLibrary.getCardFiles(),
+                assetNameLibrary.getPanelFiles(),
+                assetNameLibrary.getSheetFiles(),
+                assetNameLibrary.getObjectFiles()
+        };
     }
 
     public void load(){
-        loadTextures(assetNameLibrary.getGuiFiles());
+        MyAssetLoader loader = new MyAssetLoader(assetManager);
 
-        loadTextures(assetNameLibrary.getCardFiles());
+        for (String[] s:textureCollection)
+            loader.loadTextures(s);
 
-        loadTextures(assetNameLibrary.getPanelFiles());
+        loader.loadMusic(assetNameLibrary.getMusicFiles());
 
-        loadTextures(assetNameLibrary.getSheetFiles());
-
-        loadTextures(assetNameLibrary.getObjectFiles());
-
-        loadMusic();
-
-        loadSound();
+        loader.loadSound(assetNameLibrary.getSoundFiles());
 
         assetManager.finishLoading();
     }
@@ -36,26 +40,34 @@ public class MyAssetProcessor {
         return assetManager;
     }
 
-    private void loadTextures(String[] files){
-         for (String file : files)
-            assetManager.load(file, Texture.class);
-    }
-
-    private void loadMusic(){
-        for(String file : assetNameLibrary.getMusicFiles())
-            assetManager.load(file, Music.class);
-    }
-
-    private void loadSound(){
-        for(String file: assetNameLibrary.getSoundFiles())
-            assetManager.load(file, Sound.class);
-    }
-
     public void update(int millis){
         assetManager.update(millis);
     }
 
     public void dispose(){
         assetManager.dispose();
+    }
+}
+
+class MyAssetLoader{
+    private final AssetManager assetManager;
+
+    public MyAssetLoader(AssetManager assetManager) {
+        this.assetManager = assetManager;
+    }
+
+    public void loadTextures(String[] files){
+         for (String file : files)
+            assetManager.load(file, Texture.class);
+    }
+
+    public void loadMusic(String[] files){
+        for(String file : files)
+            assetManager.load(file, Music.class);
+    }
+
+    public void loadSound(String[] files){
+        for(String file: files)
+            assetManager.load(file, Sound.class);
     }
 }
