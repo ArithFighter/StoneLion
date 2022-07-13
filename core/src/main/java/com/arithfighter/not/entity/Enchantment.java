6a5e31b327500296bceec3ed09d8ee2941941ca6
catchball <palefever98@gmail.com>
2022-07-13 20:03:30 +0800

    add Ropes to Enchantment

--- core/src/main/java/com/arithfighter/not/entity/Enchantment.java
+++ core/src/main/java/com/arithfighter/not/entity/Enchantment.java
@@ -1,6 +1,7 @@
 package com.arithfighter.not.entity;
 
 import com.arithfighter.not.pojo.LayoutSetter;
+import com.arithfighter.not.pojo.Point;
 import com.arithfighter.not.pojo.Rectangle;
 import com.arithfighter.not.widget.SpriteWidget;
 import com.arithfighter.not.widget.VisibleWidget;
@@ -10,7 +11,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Enchantment {
     private final VisibleWidget pillarLeft;
     private final VisibleWidget pillarRight;
-    private final VisibleWidget rope;
+    private final Ropes ropes;
 
     public Enchantment(Texture[] textures){
         float scale = 8f;
@@ -18,7 +19,7 @@ public class Enchantment {
 
         pillarRight = new SpriteWidget(textures[1], scale);
 
-        rope = new SpriteWidget(textures[2], scale);
+        ropes = new Ropes(textures[2], scale);
 
         LayoutSetter layoutSetter = new LayoutSetter();
         layoutSetter.setGrid(11,2);
@@ -27,12 +28,28 @@ public class Enchantment {
 
         pillarLeft.setPosition(grid.getWidth()*1, 0);
         pillarRight.setPosition(grid.getWidth()*8, 0);
-        rope.setPosition(grid.getWidth()*2, 0);
+        ropes.setPoint(new Point(grid.getWidth()*2, 0));
+    }
+
+    public void draw(SpriteBatch batch){
+        ropes.draw(batch);
+        pillarLeft.draw(batch);
+        pillarRight.draw(batch);
+    }
+}
+
+class Ropes{
+    private final VisibleWidget rope;
+
+    public Ropes(Texture texture, float scale) {
+        rope = new SpriteWidget(texture, scale);
+    }
+
+    public void setPoint(Point point){
+        rope.setPosition(point.getX(), point.getY());
     }
 
     public void draw(SpriteBatch batch){
         rope.draw(batch);
-        pillarLeft.draw(batch);
-        pillarRight.draw(batch);
     }
 }
