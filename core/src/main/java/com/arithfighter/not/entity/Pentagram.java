d7d8c2650ff5a80ff142545f47a951bb20a2f5a2
catchball <palefever98@gmail.com>
2022-07-18 17:20:59 +0800

    update PlaceMarkCollection

--- core/src/main/java/com/arithfighter/not/entity/Pentagram.java
+++ core/src/main/java/com/arithfighter/not/entity/Pentagram.java
@@ -1,6 +1,8 @@
 package com.arithfighter.not.entity;
 
+import com.arithfighter.not.pojo.LayoutSetter;
 import com.arithfighter.not.pojo.Point;
+import com.arithfighter.not.pojo.Rectangle;
 import com.arithfighter.not.widget.SpriteWidget;
 import com.arithfighter.not.widget.VisibleWidget;
 import com.badlogic.gdx.graphics.Color;
@@ -9,25 +11,32 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Pentagram {
     private final VisibleWidget pentagram;
-    private final PlaceMark mark;
+    private final PlaceMarkCollection mark;
     private Point point;
 
     public Pentagram(Texture[] textures, float scale){
         pentagram = new SpriteWidget(textures[0], scale);
 
-        mark = new PlaceMark(textures[1], 1.5f);
-        mark.setLevel(EnchantmentLevel.LOW);
+        mark = new PlaceMarkCollection(textures[1], 1.5f);
+
+        LayoutSetter layoutSetter = new LayoutSetter(new Rectangle(
+                pentagram.getWidget().getWidth(),
+                pentagram.getWidget().getHeight()
+                ));
+        layoutSetter.setGrid(7,6);
+        mark.setGrid(layoutSetter.getGrid());
     }
 
     public void setPoint(Point point) {
-        this.point = point;
+        this.point = new Point(point.getX()-pentagram.getWidget().getWidth()/2, point.getY());
+
+        mark.setInitPoint(this.point);
     }
 
     public void draw(SpriteBatch batch){
-        pentagram.setPosition(point.getX()-pentagram.getWidget().getWidth()/2, point.getY());
+        pentagram.setPosition(point.getX(), point.getY());
         pentagram.draw(batch);
 
-        mark.setPoint(new Point(point.getX(), point.getY()));
         mark.draw(batch);
     }
 }
@@ -64,13 +73,23 @@ enum EnchantmentLevel{
 class PlaceMarkCollection{
     private final PlaceMark[] placeMarks;
     private Point initPoint;
+    private Rectangle grid;
 
     public PlaceMarkCollection(Texture texture, float scale){
         placeMarks = new PlaceMark[6];
 
         for (int i =0;i< placeMarks.length;i++){
             placeMarks[i] = new PlaceMark(texture, scale);
+            placeMarks[i].setLevel(EnchantmentLevel.MID);
         }
+    }
+
+    public PlaceMark[] getPlaceMarks() {
+        return placeMarks;
+    }
+
+    public void setGrid(Rectangle grid) {
+        this.grid = grid;
     }
 
     public void setInitPoint(Point point){
@@ -80,8 +99,17 @@ class PlaceMarkCollection{
     public void draw(SpriteBatch batch){
         for (int i =0;i< placeMarks.length;i++){
             if (i == 0)
+            placeMarks[i].setPoint(new Point(
+                    initPoint.getX()+ grid.getWidth()*3,
+                    initPoint.getY()+ grid.getHeight()*4)
+            );
+            else
                 placeMarks[i].setPoint(initPoint);
-            placeMarks[i].draw(batch);
+        }
+
+        for (PlaceMark placeMark : placeMarks) {
+            if (placeMark != null)
+                placeMark.draw(batch);
         }
     }
 }
