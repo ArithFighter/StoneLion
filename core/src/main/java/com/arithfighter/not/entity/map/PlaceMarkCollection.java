1c9bec512a68df289aeb074e04292e542c51ea30
catchball <palefever98@gmail.com>
2022-07-19 15:36:51 +0800

    update PlaceMarkCollection then test

--- core/src/main/java/com/arithfighter/not/entity/map/PlaceMarkCollection.java
+++ core/src/main/java/com/arithfighter/not/entity/map/PlaceMarkCollection.java
@@ -17,7 +17,12 @@ class PlaceMarkCollection {
 
         for (int i = 0; i < placeMarks.length; i++) {
             placeMarks[i] = new PlaceMark(texture, scale);
-            placeMarks[i].setLevel(EnchantmentLevel.MID);
+            if (i<2)
+                placeMarks[i].setLevel(EnchantmentLevel.MID);
+            else if (i<4)
+                placeMarks[i].setLevel(EnchantmentLevel.LOW);
+            else
+                placeMarks[i].setLevel(EnchantmentLevel.HIGH);
         }
 
         placeMarkPlacer = new PlaceMarkPlacer();
